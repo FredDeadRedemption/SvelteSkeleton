9685d464581538bf1c58969802143faa7e1fb177
Fred_Dead_Redemption <frederikdau@gmail.com>
2024-07-04 16:26:43 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div id="wrap">
+	<h1>Fredsta's sveltekit setup</h1>
+	<p>Visit <a href="https://kit.svelte.dev">docs</a> to read the documentation</p>
+</div>
+
+<style lang="scss">
+	#wrap {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 20px;
+	}
+	h1 {
+		font-size: 3rem;
+		color: $primary;
+	}
+	p {
+		font-size: 2rem;
+	}
+	a {
+		text-decoration: underline;
+	}
+</style>
